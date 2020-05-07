9b57220860853b988be902c1a06b9ed38b050f22
alvaropique <alvaro.pique01@estudiant.upf.edu>
2020-05-07 17:21:29 +0200

    Practica_3

---
+++ PoseNetOSC-Unity-master/Assets/extOSC/Scripts/Game/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public float gotSphereDestroyDelay;
+    private bool hitBySphere;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cuerpo") && !hitBySphere)
+        {
+            hitBySphere = true;
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitBySphere = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
